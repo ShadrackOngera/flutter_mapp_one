3179381bf53315fd7b788936297f4ad1d4d7e520
shadrackongera <shadrack.sme@gmail.com>
2023-03-22 11:44:33 +0300

    image from internet

--- lib/learn_flutter_page.dart
+++ lib/learn_flutter_page.dart
@@ -23,77 +23,80 @@ class _LearnFlutterPageState extends State<LearnFlutterPage> {
           icon: const Icon(Icons.arrow_back_ios),
         ),
       ),
-      body: Column(
-        children: [
-          Image.asset('images/one.jpg'),
-          const SizedBox(
-            height: 10,
-          ),
-          const Divider(),
-          Container(
-            margin: const EdgeInsets.all(10.0),
-            padding: const EdgeInsets.all(10.0),
-            color: Colors.blueGrey,
-            width: double.infinity,
-            child: const Center(
-              child: Text(
-                'hello',
-                style: TextStyle(color: Colors.white),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset('images/one.jpg'),
+            const SizedBox(
+              height: 10,
+            ),
+            const Divider(),
+            Container(
+              margin: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.all(10.0),
+              color: Colors.blueGrey,
+              width: double.infinity,
+              child: const Center(
+                child: Text(
+                  'hello',
+                  style: TextStyle(color: Colors.white),
+                ),
               ),
             ),
-          ),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              primary: isSwitch ? Colors.green : Colors.blue,
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: isSwitch ? Colors.green : Colors.blue,
+              ),
+              onPressed: () {
+                debugPrint('elevated button');
+              },
+              child: const Text('Elevated Button'),
             ),
-            onPressed: () {
-              debugPrint('elevated button');
-            },
-            child: const Text('Elevated Button'),
-          ),
-          OutlinedButton(
-            onPressed: () {
-              debugPrint('OutlinedButton button');
-            },
-            child: const Text('Outlined Button'),
-          ),
-          TextButton(
-            onPressed: () {
-              debugPrint('TextButton button');
-            },
-            child: const Text('Text Button'),
-          ),
-          GestureDetector(
-            behavior: HitTestBehavior.opaque,
-            onTap: () {
-              debugPrint('Row clicked');
-            },
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: const [
-                Icon(
-                  Icons.fire_extinguisher,
-                  color: Colors.red,
-                ),
-                Text('Fire Extinguiser'),
-                Icon(Icons.fire_extinguisher),
-              ],
+            OutlinedButton(
+              onPressed: () {
+                debugPrint('OutlinedButton button');
+              },
+              child: const Text('Outlined Button'),
             ),
-          ),
-          Switch(
-            value: isSwitch,
-            onChanged: (bool newBool) {
+            TextButton(
+              onPressed: () {
+                debugPrint('TextButton button');
+              },
+              child: const Text('Text Button'),
+            ),
+            GestureDetector(
+              behavior: HitTestBehavior.opaque,
+              onTap: () {
+                debugPrint('Row clicked');
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  Icon(
+                    Icons.fire_extinguisher,
+                    color: Colors.red,
+                  ),
+                  Text('Fire Extinguiser'),
+                  Icon(Icons.fire_extinguisher),
+                ],
+              ),
+            ),
+            Switch(
+              value: isSwitch,
+              onChanged: (bool newBool) {
+                setState(() {
+                  isSwitch = newBool;
+                });
+              },
+            ),
+            Checkbox(value: isCheckBox, onChanged: (bool? newBool) {
               setState(() {
-                isSwitch = newBool;
+                isCheckBox = newBool;
               });
-            },
-          ),
-          Checkbox(value: isCheckBox, onChanged: (bool? newBool) {
-            setState(() {
-              isCheckBox = newBool;
-            });
-          })
-        ],
+            }),
+            Image.network('https://i.pinimg.com/originals/7c/d7/e9/7cd7e9938329f23bf0e177d735f605c8.jpg')
+          ],
+        ),
       ),
     );
   }
